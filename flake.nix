2603978c353198fdce199c618db8a2cf87313b74
Elliott Lewandowski <elewandowski30@gmail.com>
2026-03-13 15:50:30 -0800

    Added home-manager to flake.nix

--- flake.nix
+++ flake.nix
@@ -4,18 +4,27 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {  
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
       };
     };
+    
+    homeConfigurations = {
+      wicker = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    }; 
   };
-
 }
