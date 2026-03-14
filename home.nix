929b373838895cbd91f096ed3ca5ca0b6a6e0c8e
Elliott Lewandowski <elewandowski30@gmail.com>
2026-03-13 21:38:38 -0800

    Moved waybar config into seperate file

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./user/wm/hyprland/waybar.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "wicker";
@@ -43,126 +47,6 @@
     enable = true;
   };
 
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-    style = builtins.readFile dots/waybar/style.css;
-    settings = [{
-      layer = "top";
-      position = "top";
-      mod = "dock";
-      exclusive = true;
-      passtrough = false;
-      gtk-layer-shell = true;
-      height = 0;
-      modules-left = [
-        "hyprland/workspaces"
-        "custom/divider"
-        "custom/weather"
-        "custom/divider"
-        "cpu"
-        "custom/divider"
-        "memory"
-      ];
-      modules-center = [ "hyprland/window" ];
-      modules-right = [
-        "tray"
-        "network"
-        "custom/divider"
-        "backlight"
-        "custom/divider"
-        "pulseaudio"
-        "custom/divider"
-        "battery"
-        "custom/divider"
-        "clock"
-      ];
-      "hyprland/window" = { format = "{}"; };
-      "wlr/workspaces" = {
-        on-scroll-up = "hyprctl dispatch workspace e+1";
-        on-scroll-down = "hyprctl dispatch workspace e-1";
-        all-outputs = true;
-        on-click = "activate";
-      };
-      battery = { format = "󰁹 {}%"; };
-      cpu = {
-        interval = 10;
-        format = "󰻠 {}%";
-        max-length = 10;
-        on-click = "";
-      };
-      memory = {
-        interval = 30;
-        format = "  {}%";
-        format-alt = " {used:0.1f}G";
-        max-length = 10;
-      };
-      backlight = {
-        format = "󰖨 {}";
-        device = "acpi_video0";
-      };
-      "custom/weather" = {
-        tooltip = true;
-        format = "{}";
-        restart-interval = 300;
-        exec = "/home/roastbeefer/.cargo/bin/weather";
-      };
-      tray = {
-        icon-size = 13;
-        tooltip = false;
-        spacing = 10;
-      };
-      network = {
-        format = "󰖩 {essid}";
-        format-disconnected = "󰖪 disconnected";
-      };
-      clock = {
-        format = " {:%I:%M %p   %m/%d} ";
-        tooltip-format = ''
-          <big>{:%Y %B}</big>
-          <tt><small>{calendar}</small></tt>'';
-      };
-      pulseaudio = {
-        format = "{icon} {volume}%";
-        tooltip = false;
-        format-muted = " Muted";
-        on-click = "pamixer -t";
-        on-scroll-up = "pamixer -i 5";
-        on-scroll-down = "pamixer -d 5";
-        scroll-step = 5;
-        format-icons = {
-          headphone = "";
-          hands-free = "";
-          headset = "";
-          phone = "";
-          portable = "";
-          car = "";
-          default = [ "" "" "" ];
-        };
-      };
-      "pulseaudio#microphone" = {
-        format = "{format_source}";
-        tooltip = false;
-        format-source = " {volume}%";
-        format-source-muted = " Muted";
-        on-click = "pamixer --default-source -t";
-        on-scroll-up = "pamixer --default-source -i 5";
-        on-scroll-down = "pamixer --default-source -d 5";
-        scroll-step = 5;
-      };
-      "custom/divider" = {
-        format = " | ";
-        interval = "once";
-        tooltip = false;
-      };
-      "custom/endright" = {
-        format = "_";
-        interval = "once";
-        tooltip = false;
-      };
-    }];
-
-  };
   
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
