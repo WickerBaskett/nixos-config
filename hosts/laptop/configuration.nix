bbaa53e65033a9976ac0d19f65e635232dda943e
erlewa <erlewandowski@alaska.edu>
2026-03-24 14:51:06 -0800

    Added steam to laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,6 +4,7 @@
   imports = [ 
     ../../common/configuration.nix
     ./hardware-configuration.nix
+    ../../system/gaming
   ];
   networking.hostName = "nixos-laptop"; # Define your hostname.
 }
