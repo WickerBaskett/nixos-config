120a17bd06e9a0914426ebeecef82eab39dabbcc
Elliott Lewandowski <elewandowski30@gmail.com>
2026-03-30 21:43:33 -0800

    Added steam to desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -4,6 +4,7 @@
   imports = [ 
     ../../common/configuration.nix
     ./hardware-configuration.nix
+    ../../system/gaming
   ];
 
   networking.hostName = "nixos-desktop"; # Define your hostname.
