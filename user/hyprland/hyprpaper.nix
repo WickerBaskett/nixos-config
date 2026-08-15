cbe8cc4692d6a02aa0949b16a1b8437aedfdcbd6
Elliott Lewandowski <elewandowski30@gmail.com>
2026-08-15 13:37:25 -0800

    feat: wallpaper pulled from remote

--- user/hyprland/hyprpaper.nix
+++ user/hyprland/hyprpaper.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
-
-{
+let
+  mountainLake = builtins.fetchurl {
+    url="https://i.redd.it/n1ci0trfgrey.jpg";
+    sha256 = "49c8281e2c55fb2e4efce9598c1892421e7befc86e312e82822a3afe6d2e7a57";
+    name = "mountain-lake.jpg"; 
+  };
+in {
   services.hyprpaper = {
     enable = true;
     settings = {
@@ -12,11 +17,11 @@
         "/home/wicker/wallpapers/dark-firewatch.png"
         "/home/wicker/wallpapers/forest-bridge.jpg"
         "/home/wicker/wallpapers/among-trees-river.jpg"
-        "/home/wicker/wallpapers/mountain-lake.jpg"
+        "${mountainLake}"
       ];
 
       wallpaper = [
-        ",/home/wicker/wallpapers/mountain-lake.jpg"
+        ",${mountainLake}"
       ];
     };
   };
